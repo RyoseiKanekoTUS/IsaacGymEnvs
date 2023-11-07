658df66a47a082675b5a20f712604fb2a111b489
tus7520024 <7520024@ed.tus.ac.jp>
2023-11-08 01:07:08 +0900

    minor change

--- isaacgymenvs/tasks/door_test.py
+++ isaacgymenvs/tasks/door_test.py
@@ -16,3 +16,113 @@ from .base.vec_task import VecTask
 class DoorTest(VecTask):
 
     def __init__(self, cfg, rl_device, sim_device, graphics_device_id, headless, virtual_screen_capture, force_render):
+
+        self.cfg = cfg
+
+        self.randomization_params = self.cfg["task"]["randomization_params"]
+        self.randomize = self.cfg["task"]["randomize"]
+        self.dof_vel_scale = self.cfg["env"]["dofVelocityScale"]
+        self.contact_force_scale = self.cfg["env"]["contactForceScale"]
+        self.power_scale = self.cfg["env"]["powerScale"]
+        self.heading_weight = self.cfg["env"]["headingWeight"]
+        self.up_weight = self.cfg["env"]["upWeight"]
+        self.actions_cost_scale = self.cfg["env"]["actionsCost"]
+        self.energy_cost_scale = self.cfg["env"]["energyCost"]
+        self.joints_at_limit_cost_scale = self.cfg["env"]["jointsAtLimitCost"]
+        self.death_cost = self.cfg["env"]["deathCost"]
+        self.termination_height = self.cfg["env"]["terminationHeight"]
+
+        self.debug_viz = self.cfg["env"]["enableDebugVis"]
+        self.plane_static_friction = self.cfg["env"]["plane"]["staticFriction"]
+        self.plane_dynamic_friction = self.cfg["env"]["plane"]["dynamicFriction"]
+        self.plane_restitution = self.cfg["env"]["plane"]["restitution"]
+
+        self.cfg["env"]["numObservations"] = 15
+        self.cfg["env"]["numActions"] = 6
+
+        super().__init__(config=self.cfg, rl_device=rl_device, sim_device=sim_device, graphics_device_id=graphics_device_id, headless=headless, virtual_screen_capture=virtual_screen_capture, force_render=force_render)
+
+        if self.viewer != None:
+            cam_pos = gymapi.Vec3(50.0, 25.0, 2.4)
+            cam_target = gymapi.Vec3(45.0, 25.0, 0.0)
+            self.gym.viewer_camera_look_at(self.viewer, None, cam_pos, cam_target)
+
+        actor_root_state = self.gym.acquire_actor_root_state_tensor(self.sim)
+        dof_state_tensor = self.gym.acquire_dof_state_tensor(self.sim)
+        sensor_tensor = self.gym.acquire_force_sensor_tensor(self.sim)
+
+        sensors_per_env = 4
+        self.vec_sensor_tensor = gymtorch.wrap_tensor(sensor_tensor).view(self.num_envs, sensors_per_env * 6)
+
+        self.gym.refresh_dof_state_tensor(self.sim)
+        self.gym.refresh_actor_root_state_tensor(self.sim)
+
+        self.root_states = gymtorch.wrap_tensor(actor_root_state)
+        self.initial_root_states = self.root_states.clone()
+        self.initial_root_states[:, 7:13] = 0  # set lin_vel and ang_vel to 0
+
+        # create some wrapper tensors for different slices
+        self.dof_state = gymtorch.wrap_tensor(dof_state_tensor)
+        self.dof_pos = self.dof_state.view(self.num_envs, self.num_dof, 2)[..., 0]
+        self.dof_vel = self.dof_state.view(self.num_envs, self.num_dof, 2)[..., 1]
+        self.initial_dof_pos = torch.zeros_like(self.dof_pos, device=self.device, dtype=torch.float)
+        zero_tensor = torch.tensor([0.0], device=self.device)
+        self.initial_dof_pos = torch.where(self.dof_limits_lower > zero_tensor, self.dof_limits_lower,
+                                           torch.where(self.dof_limits_upper < zero_tensor, self.dof_limits_upper, self.initial_dof_pos))
+        self.initial_dof_vel = torch.zeros_like(self.dof_vel, device=self.device, dtype=torch.float)
+
+        # initialize some data used later on
+        self.up_vec = to_torch(get_axis_params(1., self.up_axis_idx), device=self.device).repeat((self.num_envs, 1))
+        self.heading_vec = to_torch([1, 0, 0], device=self.device).repeat((self.num_envs, 1))
+        self.inv_start_rot = quat_conjugate(self.start_rotation).repeat((self.num_envs, 1))
+
+        self.basis_vec0 = self.heading_vec.clone()
+        self.basis_vec1 = self.up_vec.clone()
+
+        self.targets = to_torch([1000, 0, 0], device=self.device).repeat((self.num_envs, 1))
+        self.target_dirs = to_torch([1, 0, 0], device=self.device).repeat((self.num_envs, 1))
+        self.dt = self.cfg["sim"]["dt"]
+        self.potentials = to_torch([-1000./self.dt], device=self.device).repeat(self.num_envs)
+        self.prev_potentials = self.potentials.clone()
+
+    def create_sim(self):
+        self.up_axis_idx = 2 # index of up axis: Y=1, Z=2
+        self.sim = super().create_sim(self.device_id, self.graphics_device_id, self.physics_engine, self.sim_params)
+
+        self._create_ground_plane()
+        print(f'num envs {self.num_envs} env spacing {self.cfg["env"]["envSpacing"]}')
+        self._create_envs(self.num_envs, self.cfg["env"]['envSpacing'], int(np.sqrt(self.num_envs)))
+
+        if self.randomize:
+            self.apply_randomizations(self.randomization_params)
+    
+    def _create_ground_plane(self):
+        plane_params = gymapi.PlaneParams()
+        plane_params.normal = gymapi.Vec3(0.0, 0.0, 1.0)
+        plane_params.static_friction = self.plane_static_friction
+        plane_params.dynamic_friction = self.plane_dynamic_friction
+        self.gym.add_ground(self.sim, plane_params)
+
+    def _create_envs(self, num_envs, spacing, num_per_row):
+        lower = gymapi.Vec3(-spacing, -spacing, 0.0)
+        upper = gymapi.Vec3(spacing, spacing, spacing)
+
+        asset_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../assets')
+        asset_file = 'mjcf/door_test/lever_ur3_hook.xml'
+
+        if "asset" in self.cfg["env"]:
+            asset_file = self.cfg["env"]["asset"].get("assetFileName", asset_file)
+        
+        asset_path = os.path.join(asset_root, asset_file)
+        asset_root = os.path.dirname(asset_path)
+        asset_file = os.path.basename(asset_path)
+
+        asset_options = gymapi.AssetOptions()
+
+
+
+
+
+        
+
+

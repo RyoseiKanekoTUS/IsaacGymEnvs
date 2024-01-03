53fa624b68b312f6f1a097871c3aec1e9ed5a9b1
tus7520024 <7520024@ed.tus.ac.jp>
2024-01-03 14:20:03 +0900

    add ur3_dof_pos_prev

--- DoorHook_PPO.py
+++ DoorHook_PPO.py
@@ -149,7 +149,7 @@ class DoorHookTrainer(PPOnet):
 if __name__ == '__main__':
 
     path = None
-    path = 'skrl_runs/DoorHook/conv_ppo/24-01-01_23-37-40-336812_PPO/checkpoints/agent_62000.pt'
+    # path = 'skrl_runs/DoorHook/conv_ppo/24-01-01_23-37-40-336812_PPO/checkpoints/agent_62000.pt'
     
     DoorHookTrainer = DoorHookTrainer()
     DoorHookTrainer.eval(path)

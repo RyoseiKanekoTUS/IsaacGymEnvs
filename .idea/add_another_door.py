ea0b7db023fb2b8234f98200cf49dfe98b0ef4df
tus7520024 <7520024@ed.tus.ac.jp>
2024-01-13 22:25:48 +0900

    add wall doors into asset;

--- .idea/add_another_door.py
+++ .idea/add_another_door.py
@@ -65,10 +65,10 @@ if viewer is None:
 
 # load ball asset
 asset_root = "../assets"
-door_1 = "urdf/door_test/door_1.urdf"
-door_2 = 'urdf/door_test/door_2.urdf'
-door_1_inv = 'urdf/door_test/door_1_inv.urdf'
-door_2_inv = 'urdf/door_test/door_2_inv.urdf'
+door_1 = "urdf/door_test/door_1_wall.urdf"
+door_2 = 'urdf/door_test/door_2_wall.urdf'
+door_1_inv = 'urdf/door_test/door_1_inv_wall.urdf'
+door_2_inv = 'urdf/door_test/door_2_inv_wall.urdf'
 
 asset_options = gymapi.AssetOptions()
 asset_options.fix_base_link = True

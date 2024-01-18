3827116a8af3dc5a208e89f60e8edc559f366bbf
tus7520024 <7520024@ed.tus.ac.jp>
2024-01-19 02:19:30 +0900

    minor change

--- DoorHook_PPO.py
+++ DoorHook_PPO.py
@@ -187,15 +187,15 @@ class DoorHookTrainer(PPOnet):
         self.env = load_isaacgym_env_preview4(task_name="DoorHook")
         self.env = wrap_env(self.env)
         self.device = self.env.device
-        self.memory = RandomMemory(memory_size=256, num_envs=self.env.num_envs, device=self.device)
+        self.memory = RandomMemory(memory_size=64, num_envs=self.env.num_envs, device=self.device)
         self.models = {}
         self.models["policy"] = PPOnet(self.env.observation_space, self.env.action_space, self.device)
         self.models["value"] = self.models["policy"]  # same instance: shared model
 
         self.cfg = PPO_DEFAULT_CONFIG.copy()
-        self.cfg["rollouts"] = 256  # memory_size
-        self.cfg["learning_epochs"] = 24
-        self.cfg["mini_batches"] = 128  # 16 * 4096 / 8192
+        self.cfg["rollouts"] = 64  # memory_size
+        self.cfg["learning_epochs"] = 12
+        self.cfg["mini_batches"] = 64  # 16 * 4096 / 8192
         self.cfg["discount_factor"] = 0.99
         self.cfg["lambda"] = 0.95
         self.cfg["learning_rate"] = 5e-3

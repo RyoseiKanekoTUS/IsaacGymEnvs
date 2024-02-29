8f30defbf01ab916c5c10ff84e33015a5a2785c0
tus7520024 <7520024@ed.tus.ac.jp>
2024-02-29 21:27:04 +0900

    change reward

--- isaacgymenvs/tasks/door_hook.py
+++ isaacgymenvs/tasks/door_hook.py
@@ -35,8 +35,8 @@ class DoorHook(VecTask):
 
         # reward parameters
         self.open_reward_scale = 100.0
-        self.handle_reward_scale = 50.0
-        self.dist_reward_scale = 5.0
+        self.handle_reward_scale = 100.0
+        self.dist_reward_scale = 2.5
         self.o_dist_reward_scale = 1.0
 
         self.action_penalty_scale = 0.01

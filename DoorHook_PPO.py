7e10c6083920eeb6682364ffd0f67925ed8cf1f8
tus7520024 <7520024@ed.tus.ac.jp>
2024-03-25 11:58:29 +0900

    minor changes

--- DoorHook_PPO.py
+++ DoorHook_PPO.py
@@ -165,8 +165,8 @@ class DoorHookTrainer(PPOnet):
 if __name__ == '__main__':
 
     path = None
-    # path = '../../learning_data/DoorHook/skrl/0111_uni_pull_push_both/best_agent.pt'
-    path = 'skrl_runs/DoorHook/non_vel/24-03-19_13-04-08-617586_PPO/checkpoints/best_agent.pt'
+    path = '../../learning_data/DoorHook_POST_GRADUATION/non_vel_new_CNN_0319/agent_250000.pt'
+    # path = 'skrl_runs/DoorHook/non_vel/24-03-19_13-04-08-617586_PPO/checkpoints/best_agent.pt'
     
     DoorHookTrainer = DoorHookTrainer()
     DoorHookTrainer.eval(path)

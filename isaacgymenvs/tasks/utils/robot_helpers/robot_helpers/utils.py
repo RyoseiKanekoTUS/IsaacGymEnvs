1f0854d67b8705aca3ab3a23a092ebca6a9ed0fe
tus7520024 <7520024@ed.tus.ac.jp>
2024-07-19 19:00:11 +0900

    delete robot_helper

--- isaacgymenvs/tasks/utils/robot_helpers/robot_helpers/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def sph2cart(r, theta, phi):
-    return np.r_[
-        r * np.sin(theta) * np.cos(phi),
-        r * np.sin(theta) * np.sin(phi),
-        r * np.cos(theta),
-    ]

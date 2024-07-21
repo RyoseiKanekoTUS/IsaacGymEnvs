7b5db01cb3efee8e33aa9073df92d4bd2afb060e
tus7520024 <7520024@ed.tus.ac.jp>
2024-07-21 23:06:59 +0900

    change LR 5e-3 -> 1e-3

--- DoorHook_PPO.py
+++ DoorHook_PPO.py
@@ -140,7 +140,7 @@ class DoorHookTrainer(PPOnet):
         self.cfg["mini_batches"] = 60  # mem_size*num_envs / mini_batches : included in each mini_batch
         self.cfg["discount_factor"] = 0.99
         self.cfg["lambda"] = 0.95
-        self.cfg["learning_rate"] = 5e-3
+        self.cfg["learning_rate"] = 5e-4
         self.cfg["learning_rate_scheduler"] = KLAdaptiveRL
         self.cfg["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.008}
         self.cfg["random_timesteps"] = 0

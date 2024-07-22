bf936be0b0cd20fde4a8b2f9cf069e4ff57e45f4
kanpyo <7523530@ed.tus.ac.jp>
2024-07-22 14:42:50 +0900

    change action scale base 0.015 -> 0.02

--- isaacgymenvs/tasks/door_hook.py
+++ isaacgymenvs/tasks/door_hook.py
@@ -42,7 +42,7 @@ class DoorHook(VecTask):
         self.door_scale_rand_param = 0.1
 
         # rand param for action scales
-        self.action_scale_base = 0.015 # base # 0.025?
+        self.action_scale_base = 0.020 # base # 0.025?
         self.action_scale_rand = 0.001 # noise
 
         # rand param for start

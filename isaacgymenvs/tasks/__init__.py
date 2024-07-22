b339ce9d7755948b92a379408e363f81f7d2a901
tus7520024 <7520024@ed.tus.ac.jp>
2024-07-22 13:06:50 +0900

    clean up

--- isaacgymenvs/tasks/__init__.py
+++ isaacgymenvs/tasks/__init__.py
@@ -1,6 +1,4 @@
 from .franka_cabinet import FrankaCabinet
-from .door_test import DoorTest
-from .door_test_2 import DoorTest_2
 from .door_hook import DoorHook
 from .ur3_door_hook import UR3_DoorHook
 
@@ -10,8 +8,6 @@ from .ur3_door_hook import UR3_DoorHook
 # Mappings from strings to environments
 isaacgym_task_map = {
     "FrankaCabinet": FrankaCabinet,
-    "DoorTest": DoorTest,
-    "DoorTest_2": DoorTest_2,
     "DoorHook": DoorHook,
     "DoorHookSAC": DoorHook,
     "UR3_DoorHook": UR3_DoorHook
